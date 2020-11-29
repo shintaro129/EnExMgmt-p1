75c90b6546af6f8890387d3a5d53d23fe51fa8d2
shintaro129 <73435075+shintaro129@users.noreply.github.com>
2020-11-29 20:32:55 +0900

    Add files via upload

---
+++ scan.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+sys.path.insert(1, os.path.split(sys.path[0])[0])
+import binascii
+import nfc
+import datetime
+import json
+import collections as cl
+
+CACHE = "/home/test/EnterID.dat"
+
+def logRecord(scantime,check,scanID):
+    LOGFILE = "/home/test/log/log"+scantime[:10]+".dat"
+    wlog = open(LOGFILE,"a")
+    log = scantime+","+str(check)+","+scanID+"\n"
+    wlog.write(log)
+    wlog.close()
+    
+
+def checkRecord(scanID):
+    rlog = open(CACHE,"r")
+    Enterlog = set(map(str,rlog.readline().split(",")))
+    rlog.close()
+
+    checkEntered = bool()
+    if(scanID in Enterlog):
+        Enterlog.remove(scanID)
+        checkEntered = False
+    else:
+        Enterlog.add(scanID)
+        checkEntered = True
+
+    rlog = open(CACHE,"w")
+    rlog.writelines(",".join(Enterlog))
+    rlog.close()
+
+    return checkEntered
+
+
+def connected(tag):
+  if isinstance(tag, nfc.tag.tt3.Type3Tag):
+    try:
+        service_code = 0x09CB
+        scantime = "".join(list(map(str,str(datetime.datetime.now())))[:19])
+        sc = nfc.tag.tt3.ServiceCode(service_code >> 6 ,service_code & 0x3f)
+        bc = nfc.tag.tt3.BlockCode(0,service=0)
+        data = tag.read_without_encryption([sc],[bc])
+        scanID = data[2:10].decode("utf-8")
+        check = checkRecord(scanID)
+        print(scantime,check,scanID)
+        logRecord(scantime,check,scanID)
+        #ここで音声鳴らす
+        if(check):
+            #入室
+            print("Enter")
+        else:
+            #退室
+            print("Exit")
+
+
+    except Exception as e:
+      print("error: %s" % e)
+  else:
+    print("error: tag isn't Type3Tag")
+
+if __name__ == "__main__":
+    clf = nfc.ContactlessFrontend('usb:054c:06c3')
+    clf.connect(rdwr={'on-connect': connected})
